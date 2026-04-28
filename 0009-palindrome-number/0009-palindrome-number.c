2cc5272c6002d1897f8e15e0651a42e26175cf9a
Kishore M <mrmks46007@gmail.com>
2026-04-28 22:04:10 +0530

    Time: 21 ms (5.09%), Space: 8.1 MB (99.71%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,10 @@
+bool isPalindrome(int x) {
+    if (x<0 || x!=0 && x%10==0) return false;
+    int c=0;
+    while(x>c){
+        int d=x%10;
+        c=(c*10)+d;
+        x/=10;}
+        printf("%d",c);
+    return (c==x || x==c/10);
+}
