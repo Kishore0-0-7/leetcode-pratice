0b1d1e871262bd27670437b09c95ef77efccefd9
Kishore M <mrmks46007@gmail.com>
2026-06-13 14:35:40 +0530

    Time: 17 ms (61.16%), Space: 12.5 MB (21.49%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mapWordWeights(self, words, weights):
+        """
+        :type words: List[str]
+        :type weights: List[int]
+        :rtype: str
+        """
+        ans=""
+        for word in words:
+            c=0
+            for ch in word:
+                c+=weights[ord(ch)-ord('a')]
+            ans+=(chr(ord('z')-(c%26)))
+        return ans
